12db59dcbb56a485d384de07b1659247f7db0806
paulo-gouvea <paulogouvea1994@gmail.com>
2025-05-28 20:01:48 -0300

    feat: starting home page

---
+++ src/components/input-box.tsx
@@ -0,0 +1,7 @@
+export function InputBox(){
+    return (
+        <div className="w-[40%] h-[380px] rounded-md bg-white max-md:w-[100%] max-md:h-[316px] max-md:mb-[12px]">
+
+        </div>
+    )
+}
---
+++ src/components/link-box.tsx
@@ -0,0 +1,7 @@
+export function LinkBox(){
+    return (
+        <div className="w-[55%] h-[234px] rounded-md bg-white max-md:w-[100%] max-md:min-h-[214px] max-md:max-h-[348px]">
+
+        </div>
+    )
+}
