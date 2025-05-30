dc35b75df7e06a950cf9fd4d92a3072fb1ac4cfc
paulo-gouvea <paulogouvea1994@gmail.com>
2025-05-30 09:25:28 -0300

    feat: listing all links

--- src/components/link-box.tsx
+++ src/components/link-box.tsx
@@ -1,8 +1,17 @@
 import { ExportCsvButton } from "./export-csv-button";
 import { NoLinksContainer } from "./no-links-container";
 import { Link } from "./link";
+import { useQuery } from "@tanstack/react-query";
+import { getAllLinks } from "../api/get-all-links";
 
 export function LinkBox(){
+    const { data: links } = useQuery({
+        queryKey: ['links'],
+        queryFn: getAllLinks
+    })
+
+    console.log('data ===> ', links)
+
     return (
         <div className="w-[55%] min-h-[234px] max-h-[552px] overflow-y-auto rounded-md bg-white max-md:w-[100%] max-md:min-h-[214px] max-md:max-h-[348px] max-md:p-[24px] p-[32px]">
             <header className="w-[100%] flex items-center justify-between">
@@ -15,14 +24,18 @@ export function LinkBox(){
 
             {/* <NoLinksContainer /> */}
 
-            <Link />
-            <Link />
-            <Link />
-            <Link />
-            <Link />
-            <Link />
-            <Link />
-            <Link />
+            {
+                links?.links.length === 0 ? <NoLinksContainer /> : links?.links.map((element) => {
+                    return (
+                        <Link 
+                            key={element.id}
+                            shortURL={element.shortURL}
+                            originalURL={element.originalURL}
+                            accessCount={element.accessCount}
+                        />
+                    )
+                })
+            }
         </div>
     )
 }
--- src/components/link.tsx
+++ src/components/link.tsx
@@ -2,18 +2,28 @@ import { Link as LinkIcon } from "react-router-dom";
 import { IconButton } from "./icon-button";
 import { CopyIcon, TrashIcon } from "@phosphor-icons/react";
 
-export function Link(){
+export interface LinkProps {
+    shortURL: string
+    originalURL: string
+    accessCount: number
+}
+
+export function Link({
+    shortURL,
+    originalURL,
+    accessCount
+}: LinkProps){
     return(
         <>
             <div>
                 <div className="flex items-center justify-between">
                     <div>
-                        <LinkIcon to='#' className="text-blue-base text-Text-md font-Text-md">brev.ly/Linkedin-profile</LinkIcon>
-                        <p className="text-gray-500 text-Text-sm font-Text-sm">linkedin.com/in/myprofile</p>
+                        <LinkIcon to={originalURL} className="text-blue-base text-Text-md font-Text-md">{`brev.ly/${shortURL}`}</LinkIcon>
+                        <p className="text-gray-500 text-Text-sm font-Text-sm">{originalURL?.slice(8)}</p>
                     </div>
 
                     <div className="text-gray-500 text-Text-sm font-Text-sm flex">
-                        <p className="flex items-center mr-[15px]">30 acessos</p>
+                        <p className="flex items-center mr-[15px]">{`${accessCount} acessos`}</p>
 
                         <div className="w-[67px] flex justify-between">
                             <IconButton
